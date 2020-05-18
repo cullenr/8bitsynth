b7f50e65cad1b4d241dd5135843fbd6408ab97f6
cullenr <rory.cullen@imagination.com>
2020-05-18 13:13:52 +0100

    ADDED working additive oscilator

--- additive-osc.c
+++ additive-osc.c
@@ -7,21 +7,31 @@
 #define PI  M_PI
 #define PI2 (M_PI * 2)
 
+#ifndef DEBUG
+#define DEBUG 0
+#else
+#define DEBUG 1
+#endif
+
+#define dbg(fmt, ...) \
+    do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
+                            __LINE__, __func__, __VA_ARGS__); } while (0)
+
 enum wave_type {SIN, SQR, SAW, TRI};
 
 /**
- *  Fills the buffer with a given note
+ *  Fills the buffer with a sine wave at the given frequency
  */
 void 
-tone(const double sample_rate, const double freq, const uint8_t amplitude,
-     char *const buf, const size_t len)
+sine(const double sample_rate, const double freq, const double amplitude,
+     float *const buf, const size_t len)
 {
-    fprintf(stderr, "tone: %f %d\n", freq, amplitude);
+    dbg("sine: %f %f\n", freq, amplitude);
     const double step = PI2 * freq / sample_rate;
-    double phase = 0;
+    double phase = PI;
 
     for (int i = 0; i < len; i++) {
-        buf[i] += ((sin(phase) + 1.0) / 2.0) * amplitude;
+        buf[i] += sin(phase) * amplitude;
 
         phase += step;
         while (phase > PI2) 
@@ -29,66 +39,110 @@ tone(const double sample_rate, const double freq, const uint8_t amplitude,
     }
 }
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-    const double duration      = 1;    // seconds of audio
-    const uint32_t sample_rate = 8000; // samples per second
-    const uint32_t nyquist     = sample_rate / 2;
-    const uint32_t num_samples = duration * sample_rate;
+    uint32_t sample_rate    = 8000; // samples per second
+    double duration         = 1;    // seconds of audio
+    double freq             = 440;  // note frequency
+    enum wave_type wave     = SIN;  // wave shape
+    uint32_t opt_partials   = 0;    // user override for partial count
+    int opt;                        // getopts option
 
-    double freq = 440;
-    enum wave_type wave = SIN;
-    int opt;
-
-    while ((opt = getopt(argc, argv, "f:sqtw")) != -1) {
+    while ((opt = getopt(argc, argv, "d:f:r:p:sqtw")) != -1) {
         switch (opt) {
-            case 'f': freq = strtod(optarg, NULL); break;
+            case 'd': duration     = strtod(optarg, NULL); break;
+            case 'f': freq         = strtod(optarg, NULL); break;
+            case 'r': sample_rate  = atoi(optarg);         break;
+            case 'p': opt_partials = atoi(optarg);         break;
             case 's': wave = SIN; break;
             case 'q': wave = SQR; break;
             case 't': wave = TRI; break;
             case 'w': wave = SAW; break;
             default:
-                fprintf(stderr, "Usage: %s [-f note hz] [-s][-q][-t][-w]\n",
-                        argv[0]);
+                fprintf(stderr, "Usage: %s [-d duration seconds] " 
+                        "[-f note hz] [-r sample rate hz] [-p partials n] "
+                        "[-s|-q|-t|-w]\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
 
-    //const uint16_t maximal_partial = sample_rate - (nyquist % freq);
-    const uint32_t num_partials = sample_rate / freq;
-    uint8_t *buf = calloc(num_samples, sizeof(uint8_t));
+    const uint32_t nyquist  = sample_rate / 2;
+    const uint32_t samples  = duration * sample_rate;
+    float *const buf        = calloc(samples, sizeof(float));
 
-    fprintf(stderr, "partials: %d\n", num_partials);
-    // we could count down from the highest partial and accumuilate amplitudes
-    // that way?
-    if (wave == SIN) {
-        // Sin waves have no partials so we handle them separately to the other
-        // shapes.
-        tone(sample_rate, freq, 255, buf, num_samples); 
-    } else {
-        // These waveforms all have partials. Add each partial to the output
-        // buffer one at a time.
-        for(int i = 1; i < num_partials + 1; i++) {
-            switch(wave) {
-                case SQR:
-                    // multiply by 4/pi to normalise
-                    break;
-                case TRI: 
-                    break;
-                case SAW:
-                    // we multiply our amplitude weightings by half of the max
-                    // values as the sum of 1/2, 1/3, 1/4... approaches 1 plus
-                    // the weighting of 1 for the fundamental frequency.
-                    tone(sample_rate, i * freq, (1.0 / i) * 127, buf, num_samples); 
-                    break;
-                default:
-                    fprintf(stderr, "unknown wave type! %d", wave);
-                    exit(EXIT_FAILURE);
+    switch(wave) {
+        case SIN:
+            sine(sample_rate, freq, 1.0, buf, samples); 
+            break;
+        case SQR:
+            {
+                // unless overriden, square waves contain only odd harmonics 
+                const uint32_t partials = opt_partials > 0 
+                    ? opt_partials 
+                    : nyquist / freq / 2;
+
+                // note we drop the highest partial
+                for (int k = 1; k < partials; k++) {
+                    // calculate the mode
+                    const uint32_t n = 2 * k - 1;
+                    const float amp = 1.0 / n;
+                    sine(sample_rate, n * freq, (2.0 /PI) * amp, buf, samples); 
+                }
+                break;
             }
-        }
+            break;
+        case TRI:
+            {
+                // unless overriden, triangle waves contain only odd harmonics 
+                const uint32_t partials = opt_partials > 0 
+                    ? opt_partials 
+                    : nyquist / freq / 2;
+
+                // note we drop the highest partial
+                for (int k = 1; k < partials; k++) {
+                    // calculate the mode
+                    const uint32_t n = 2 * k - 1;
+                    // we must invert the phase of every other harmonic
+                    const float amp = k % 2
+                        ? -1.0 / (n * n)        // odd
+                        : 1.0 / (n * n);        // even
+
+                    sine(sample_rate, n * freq, (2.0 / PI) * amp, buf, samples); 
+                }
+                break;
+            }
+        case SAW:
+            {
+                // unless overriden, sawtooth waves contain odd and even
+                // harmonics.
+                const uint32_t partials = opt_partials > 0 
+                    ? opt_partials 
+                    : nyquist / freq;
+
+                // note we drop the highest partial
+                for (int k = 1; k < partials; k++) {
+                    // phase should invert for each odd harmonic. We can
+                    // effectivly do this by inverting the amplitude.
+                    // Switching the assignments here changes the sawtooth
+                    // direction.
+                    const float amp = k % 2
+                        ? -1.0 / k        // odd
+                        : 1.0 / k;        // even
+
+                    // the amplitude is further modified according to the
+                    // fourier series.
+                    sine(sample_rate, k * freq, (2.0 / PI) * amp, buf, samples); 
+                }
+                break;
+            }
+        default:
+            fprintf(stderr, "unknown wave type! %d", wave);
+            exit(EXIT_FAILURE);
     }
 
-    write(1, buf, num_samples);
+    write(1, buf, sizeof buf[0] * samples);
+    free(buf);
 
     return 0;
 }
